fbf06042bfff86480df3429b3e85bd2ddc3003d1
Corneille <85963064+Corneille9@users.noreply.github.com>
2023-10-24 14:49:58 +0100

    Update digit_easy_pay_flutter.dart

--- lib/digit_easy_pay_flutter.dart
+++ lib/digit_easy_pay_flutter.dart
@@ -5,4 +5,5 @@ export 'package:digit_easy_pay_flutter/src/common/payment_l10n.dart';
 export 'package:digit_easy_pay_flutter/src/common/payment_theme.dart';
 export 'package:digit_easy_pay_flutter/src/common/digit_easy_pay_config.dart';
 export 'package:digit_easy_pay_flutter/src/common/exceptions.dart';
+export 'package:digit_easy_pay_flutter/src/providers/payment_status_stream_manager.dart';
 export 'package:digit_easy_pay_flutter/src/digit_easy_pay.dart';
